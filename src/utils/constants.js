5d33f6c5e155a177352886fcdca3e30f7ca2928d
SayyedAquib <sayyed.aquib89@gmail.com>
2023-12-12 15:08:03 +0530

    env file added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyDaajN0jChgIgEHwUkVTbzs3x40R4gMm2A";
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 
 export const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + GOOGLE_API_KEY;
